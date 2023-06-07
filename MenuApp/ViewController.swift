09bfd5c2f93f3ea7e1b009f6c54db8143fc20e46
rampafan05 <110806636+rampafan05@users.noreply.github.com>
2023-06-07 10:12:52 +0300

    Initial Commit

---
+++ MenuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuApp
+//
+//  Created by macbook on 07.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
